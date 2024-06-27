12a48ce786dc0dd4f6f241fd926b95d96a858c16
sha <shajini.h@gmail.com>
2024-06-27 17:05:09 +0800

    clean up craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -10,25 +10,5 @@ module.exports = {
       "@": path.resolve(__dirname, "src/")
     }
   },
-  plugins: [
-    // {
-    //   plugin: CracoLessPlugin,
-    //   options: {
-    //     lessLoaderOptions: {
-    //       lessOptions: {
-    //         modifyVars: {
-    //           "@primary-color": "#038fde"
-    //         },
-    //         javascriptEnabled: true
-    //       }
-    //     },
-    //     modifyLessRule: (lessRule, context) => {
-    //       lessRule.use = lessRule.use.filter(
-    //         i => !i.loader.includes("resolve-url-loader")
-    //       );
-    //       return lessRule;
-    //     }
-    //   }
-    // }
-  ]
+  plugins: []
 };
